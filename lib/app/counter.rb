93863bbed7c40e3bc740e0df1e493a685b4b907c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-03-16 18:35:14 +0100

    Add Counter

---
+++ lib/app/counter.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# TODO: better name
+class App::Counter
+  module HashDigPatch
+    refine Hash do
+      def dig!(*keys) = keys.reduce(self) { _1.fetch(_2) }
+    end
+  end
+
+  using HashDigPatch
+
+  def initialize
+    @state = {}
+  end
+
+  def include?(key) = @state.key?(key)
+  def counter(key) = @state.dig!(key, :counter)
+
+  # Only payload of the first add is stored
+  def add(key, payload = nil)
+    item = @state[key] ||= { counter: 0, payload: }
+    item[:counter] += 1
+  end
+
+  def get(key) = @state.dig!(key, :payload)
+
+  def remove(key)
+    payload = get(key)
+    @state[key][:counter] -= 1
+    return unless counter(key).zero?
+
+    @state.delete(key)
+    payload
+  end
+end
